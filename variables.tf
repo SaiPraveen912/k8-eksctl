8e8646974cd56c947b5e982f5e89258a4e8bdb9e
SaiPraveen912 <saipraveen.immanni@gmail.com>
2024-09-10 15:50:42 +0530

    eksctl commands docker installs placed in workstation.sh and workstation.tf file

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "sg_id" {
+  type        = string
+  default     = "sg-034dbb60a2eaae2e0"
+  description = "description"
+}
+
+variable "public_subnet_id" {
+    type = string
+    default = "subnet-0a840df9ef883ea4c"
+}
